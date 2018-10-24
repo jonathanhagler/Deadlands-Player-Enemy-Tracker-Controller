50b8f5212b30bcaef164d7c5d65e658f3aceef47
jonathanhagler <20130524+jonathanhagler@users.noreply.github.com>
2018-10-24 13:31:03 -0500

    Update

---
+++ deadlands_character_tracker.ino
@@ -0,0 +1,13 @@
+static byte sevenSegmentNumbers[11] = {B00111111,B00000110,B01011011,
+                                       B01001111,B01100110,B01101101,
+                                       B01111101,B00000111,B01111111,
+                                       B01101111,B00000000}; // digits
+                                       // coorispond to the array index
+                                       // with 11 being all off
+void setup {
+
+}
+
+void loop {
+
+}
